11f52a0f6591fa9ff3ea75cec61728582eaec323
Jesse Sanford <jessesanford@gmail.com>
2012-01-13 14:39:08 -0500

    another trace

--- install.js
+++ install.js
@@ -7,6 +7,6 @@ process.stdout.write("=                                                         
 process.stdout.write("================================================================================\n");
 
 //Symlink default strategies
-process.stdout.write(JSON.stringify(process.env.npm_package_dependencies));
+process.stdout.write('dependencies looked like: '+require('util').inspect(process.env.npm_package_dependencies)+'\n');
 fs.symlinkSync('../node_modules/daida-local', './strategies/local', 'dir');
 fs.symlinkSync('../node_modules/daida-beanstalk', './strategies/beanstalk', 'dir');
