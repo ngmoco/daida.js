8ee6285e0797c20877544ab1bab8ef415558d2d8
Jesse Sanford <jessesanford@gmail.com>
2012-01-13 14:30:52 -0500

    fixed install command

--- install.js
+++ install.js
@@ -1,6 +1,12 @@
 var fs = require('fs');
 
+process.stdout.write("================================================================================\n");
+process.stdout.write("=                                                                              =\n");
+process.stdout.write("=               Daida.js Creating symlinks for default strategies              =\n");
+process.stdout.write("=                                                                              =\n");
+process.stdout.write("================================================================================\n");
+
 //Symlink default strategies
-console.log(JSON.stringify(npm_package_dependencies));
+process.stdout.write(JSON.stringify(npm_package_dependencies));
 fs.symlinkSync('../node_modules/daida-local', './strategies/local', 'dir');
 fs.symlinkSync('../node_modules/daida-beanstalk', './strategies/beanstalk', 'dir');
